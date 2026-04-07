dcdd966a54f8a98057a0fa315a5fcbeb028a905f
atul05atul-svg <atul05atul@gmail.com>
2026-04-07 15:35:06 +0530

    Add basic calculator functionality in script

--- Calculatoer.sh
+++ Calculatoer.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+echo"Enter First Number:"
+read num1
+
+echo "Enter Second Number :"
+read num2
+
+echo "Enter Third Number :"
+read num3
+
+case $op in 
++0
+  result=$((num1+num2+num3))
+  echo "Result: $result"
+  ;;
+  -)
